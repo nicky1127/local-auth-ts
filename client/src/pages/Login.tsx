c1c840e3f755cfe21621207b3d9c253f05fc7c5a
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-11-30 19:16:31 +0000

    minor change

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import Axios from "axios";
+import React, { useState } from "react";
 
 export default function Login() {
-  return <div></div>;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    Axios.post("http://localhost:4000/login", {
+      username,
+      password,
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const getUser = () => {
+    Axios.post("http://localhost:4000/user", {
+      withCredential: true,
+    }).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={login}>Login</button>
+      <button onClick={getUser}>Get User that's Logged in</button>
+    </div>
+  );
 }
