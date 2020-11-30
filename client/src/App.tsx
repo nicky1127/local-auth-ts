f5dfe9589608598966f88f79fda5571417e44a18
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-11-30 18:49:44 +0000

    add front end code and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact componet={HomePage} />
+        <Route path="/admin" exact componet={AdminPage} />
+        <Route path="/login" exact componet={Login} />
+        <Route path="/profile" exact componet={Profile} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
