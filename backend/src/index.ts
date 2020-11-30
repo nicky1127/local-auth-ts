4043a7cec6034d5bf2bb99a72bed32af89f5d9e4
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-11-30 15:57:06 +0000

    add gitignore for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import passport from "passport";
+import passportLocal from "passport-local";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+
+
