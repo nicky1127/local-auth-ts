172e042af9429244ef3c4be3beaa04ba581981d3
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-11-30 16:35:55 +0000

    create session and passport middlewares and User schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,8 @@ import session from "express-session";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 
+import User from './User'
+
 mongoose.connect("mongodb+srv://admin-user:Nnwakefield@cluster0.v57xd.mongodb.net/<dbname>?retryWrites=true&w=majority", {
 	useCreateIndex: true,
 	useNewUrlParser: true,
@@ -17,4 +19,18 @@ mongoose.connect("mongodb+srv://admin-user:Nnwakefield@cluster0.v57xd.mongodb.ne
 	console.log('Connected to Mongo',)
 })
 
+//Middleware
 
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(
+	session({
+		secret: "secretcode",
+		resave: true,
+		saveUninitialized: true
+	})
+)
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session);
