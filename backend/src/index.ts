d706b720be4972f8271fca17a4d3f8ddd015ee52
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-11-30 16:56:20 +0000

    create route register

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import mongoose, { Error } from "mongoose";
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import passport from "passport";
 import passportLocal from "passport-local";
@@ -33,4 +33,36 @@ app.use(
 )
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(passport.session);
+app.use(passport.session());
+
+
+//Route
+app.post('/register', async (req: Request, res: Response) => {
+	const { username, password } = req?.body
+
+	if (!username || !password || typeof username !== 'string' || typeof password !== 'string') {
+		res.send("Improper values");
+		return;
+	}
+
+	User.findOne({ username }, async (err: Error, doc) => {
+		if (err) throw err;
+		if (doc) res.send('User Already Exists')
+		if (!doc) {
+
+			const hashedPassword = await bcrypt.hash(password, 10)
+			const newUser = new User({
+				username,
+				password: hashedPassword
+			})
+
+			await newUser.save()
+			res.send("Success")
+		}
+	})
+
+})
+
+app.listen(4000, () => {
+	console.log('Server started')
+})
