6923b5dc440d7f98a269533ba9706ab0b28558d6
Nicky Lai <nicky.lai@lloydsbanking.com>
2020-11-30 16:18:12 +0000

    Call mongo cloud

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Error } from "mongoose";
 import express from "express";
 import cors from "cors";
 import passport from "passport";
@@ -7,5 +7,14 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
+
+mongoose.connect("mongodb+srv://admin-user:Nnwakefield@cluster0.v57xd.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+	useCreateIndex: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}, (err: Error) => {
+	if (err) throw err;
+	console.log('Connected to Mongo',)
+})
 
 
